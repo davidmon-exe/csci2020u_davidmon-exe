a9b0ab2263bef9891471a02fac1386a66c1c114d
davidmon-exe <david.mongeon@ontariotechu.net>
2021-01-26 20:49:51 -0500

    Made a build.gradle and SimpleTest.java. Did steps 1-9 and ran fine.

--- lab02/src/main/java/SimpleTest.java
+++ lab02/src/main/java/SimpleTest.java
@@ -1,7 +1,7 @@
-package main.java;
-
-public class SimpleTest {
-    public static void main(String []args){
-        System.out.println("Hello World");
+public class SimpleTest{
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
     }
+
+
 }

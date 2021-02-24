42836c4c3be540538f70a1b022691676179ebcf7
davidmon-exe <david.mongeon@ontariotechu.net>
2021-02-24 15:40:32 -0500

    Lab completed, still need to make a pdf

--- lab04-fxml/src/sample/Main.java
+++ lab04-fxml/src/sample/Main.java
@@ -12,7 +12,7 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("registration.fxml"));
         primaryStage.setTitle("Lab 4  -  FXML");
-        primaryStage.setScene(new Scene(root, 750, 350));
+        primaryStage.setScene(new Scene(root, 775, 400));
         primaryStage.show();
     }
 
